d876f2ea2be708f0821c4fd31dff1a50303050e7
renju99 <mails4ranjith@gmail.com>
2025-07-19 18:14:12 +0400

    section added on job plans

---
+++ odoo17/addons/facilities_management/models/maintenance_workorder_section.py
@@ -0,0 +1,19 @@
+from odoo import models, fields
+
+class MaintenanceWorkorderSection(models.Model):
+    _name = 'maintenance.workorder.section'
+    _description = 'Work Order Section'
+    _order = 'sequence, id'
+
+    name = fields.Char(string='Section Name', required=True)
+    sequence = fields.Integer(string='Sequence', default=10)
+    workorder_id = fields.Many2one(
+        'maintenance.workorder',
+        string='Work Order',
+        required=True,
+        ondelete='cascade'
+    )
+    task_ids = fields.One2many(
+        'maintenance.workorder.task', 'section_id',
+        string='Tasks'
+    )
